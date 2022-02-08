49c25478e83c0e0c5a3f42ea7ae2baf40a87f525
Jeffe Otto <jeffe.otto@gmail.com>
2022-02-08 11:22:01 +0100

    Unnecessary echos

--- wp-content/themes/easy/php-exercice/answers.php
+++ wp-content/themes/easy/php-exercice/answers.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Displays:';
 		
-          echo '$a string:'; 
+        echo '$a string:'; 
           //display word conditionally
            $a = '';
            $cond = false;
@@ -11,13 +11,10 @@ echo 'Displays:';
            }else{
            echo  $a = "!True Word"; 
            }
-          
            echo '<br>';
            echo '<br>';
            echo 'Switch case:';
-          
-          
-           
+
            //display word conditionally with while
            $i = '';
             switch ($i) {
@@ -30,35 +27,27 @@ echo 'Displays:';
             default:
                   echo "Is";
             }
-           
             echo '<br>';
             echo '<br>';
             echo 'For Loop:';
-           
-           
-           
+
             //display 1 to 10 with for loop
             for($i=0;$i<=10;$i++){
                   echo $i;
             }
-            
             echo '<br>';
             echo '<br>';
             echo 'While Loop:';
-           
-            
 
             //display 1 to 10 with while loop
             $i = 1;
             while ($i <= 10) {
                 echo $i++; 
             }
-
             echo '<br>';
             echo '<br>';
             echo 'While Loop Function:';
-          
-            
+
             //display 1 to 10 with while loop in a function with no parameters
             function iterate_no_params(){
                     $i = 1;
@@ -66,14 +55,11 @@ echo 'Displays:';
                         echo $i++; 
                     }
             }
-            echo iterate_no_params();
-            
-            
+            iterate_no_params();
             echo '<br>';
             echo '<br>';
             echo 'While Loop Function With Parameters:';
-            
-           
+
             //display 1 to 10 with while loop in a function with no parameters
             function iterate_params($max){
                     $i = 1;
@@ -81,85 +67,73 @@ echo 'Displays:';
                         echo $i++; 
                     }
             }
-            echo iterate_params(20);
-
-            
+            iterate_params(20);
 
             //Set variable array to next exercise
             $ex_array = array(5, 12, 10, 21, 2, 6, 8, 20, 14, 9, 12);
-            
+
             echo '<br>';
             echo '<br>';
             echo 'Arrays with comas:';
 
-            
-            
             //display $ex_array separated by comas
             function array_with_comas($array){
-              return implode(',', $array);
+              echo implode(',', $array);
             }
-           echo array_with_comas($ex_array);
-
+            array_with_comas($ex_array);
             
-           echo '<br>';
+            echo '<br>';
             echo '<br>';
             echo 'Average number from array:';
-            
 
             //echo average number from $ex_array
             function average_number_from_array($array){
               $average = array_sum($array) / count($array);
-              return round($average,2);
-              echo '<br>';
-              echo 'or';
-              echo '<br>';
-              return ceil($average);
+              echo round($average,2);
+            
+              echo ' or ';
+             
+              echo ceil($average);
             }
 
-            echo average_number_from_array($ex_array);
-
-            
+             average_number_from_array($ex_array);
             
             echo '<br>';
             echo '<br>';
             echo 'Total numbers equal greater than 10:';
-            
+
             function greaterequal($var)
             {
                 // returns whether the input id >= 10
                 return $var >= 10;
             }
-            
+
             //return total items from $ex_array >=10
             function total_items($array){
                $array_total = array_filter($array, "greaterequal");
-               return sizeof( $array_total);
+               echo sizeof( $array_total);
             }
-            echo total_items($ex_array);
-
-
+            total_items($ex_array);
+            
             echo '<br>';
             echo '<br>';
             echo 'Is 20 in the array?:';
-            
+
             //return if value is in $ex_array
             function is_in_array($array,$value){
             if (in_array($value, $array)) {
                   echo 'Yes '. $value.' is in array';
                     }
             }
-            echo is_in_array($ex_array,20);
-
-
+            is_in_array($ex_array,20);
 
             echo '<br>';
             echo '<br>';
             echo 'Largest number in array:';
-            
+
             //return largest value of $ex_array
             function largest_number($array){
-               return max($array);
+              echo max($array);
             }
-            
-            echo largest_number($ex_array);
-            
+
+            largest_number($ex_array);
